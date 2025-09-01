b37838f78ff54af4628a150a4422efdad1a708f1
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-01 10:34:56 -0500

    feat: initial commit for webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,  // transpile .js and .jsx files
+                exclude: /node_modules/,  // exclude node_modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
