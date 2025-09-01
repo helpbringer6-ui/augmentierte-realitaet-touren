e9ccbd77e05c74a82a94ca4cbfe155282899d6d6
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-01 10:34:57 -0500

    feat: initial commit for src/index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h2>Entdecken Sie mit Augmentierter Realität!</h2>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
